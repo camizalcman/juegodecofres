c8da0750895f4c1901b54ee291957065ba34fd1c
camizalcman <camizalcman@gmail.com>
2025-02-10 18:48:49 -0300

    Cambios descripciones

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,21 +5,25 @@ const modal=document.getElementById("reglas"); //Traigo el contenedor del modal
 function mostrarReglas(){
 
     console.log("Funcion mostrar reglas fue llamado")
-    modal.innerHTML = "";//Limpio el contenido del modal
-    modal.classList.add("modalReglasClases","padding1" ,"jakarta", "absolute"); //Le agrego las clases
+    modal.innerHTML = "";//Limpio el contenido del modal para que no se encime
+    modal.classList.add("modalReglasClases","padding1" ,"jakarta", "absolute", "w100m"); //Le agrego las clases
 
+    //div contenedor    
     let divContenedor=document.createElement("div");
-            
+    
+    //titulo
     let titulo=document.createElement("h3");
     titulo.innerText=("Reglas del juego");
     divContenedor.appendChild(titulo);
 
+    //descripcion
     let descripcion=document.createElement("p");
     descripcion.innerText=("En este desafío te enfrentarás a una serie de cofres repartidos en un tablero. Tres de estos cofres esconden tesoros que suman 20 puntos, mientras que otros 3 contienen trampas que restan 15 puntos y los restantes están vacíos. En cada partida, debes abrir exactamente 4 cofres. Al finalizar, ganarás si acumulas 40 puntos o más. ¡Buena suerte!");
     descripcion.style.lineHeight = "1.5"
     descripcion.classList.add("marginTop1"); //Le agrego las clases
     divContenedor.appendChild(descripcion);
 
+    //icono cruz
     let cruz=document.createElement("i");
     cruz.classList.add("fa-solid", "fa-2x","fa-xmark", "colorNaranja", "claseCruz");
     divContenedor.appendChild(cruz);
@@ -27,32 +31,32 @@ function mostrarReglas(){
     modal.appendChild(divContenedor);
     modal.showModal();
 
+    //cerrar el modal
     function hideModal(){
         console.log("hideModal llamado")
         modal.close()
     }
 
+    //le asigno el evento al icono cruz
     cruz.addEventListener("click", hideModal)
 
 }
 
+//le asigno el evento al boton mostrar reglas
 botonReglas.addEventListener("click", mostrarReglas)
 
-
+//funcion para guardar el valor ingresado en el formulario y para redirigir a la siguiente pagina
 function envioFormulario(event) {
     event.preventDefault(); // Prevenir el envío del formulario
 
     const nombre = document.getElementById("name").value; // Obtener el nombre ingresado
-    
-    if (nombre === "") {
-        alert("Por favor, ingresa tu nombre."); 
-    }
     
     localStorage.setItem("nombre", nombre); // Guardar el nombre en localStorage
 
     window.location.href = "juego.html"; // Redirigir a la siguiente página
 }
 
+//le asigno el evento al formulario
 const form = document.getElementById("formulario");
 form.addEventListener("submit", envioFormulario);
 
--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,85 +1,11 @@
-const cajaCofres=document.getElementById("contenedorCofres"); 
-cajaCofres.classList.add("centerX", "centerY");
-let cofresAbiertos=0
-let puntosTotales=0
+const cajaCofres=document.getElementById("contenedorCofres"); //me traigo el contenedor de los cofres
+cajaCofres.classList.add("centerX", "centerY"); //le agrego clases
+
+let cofresAbiertos=0 //para llevar la cuenta de cuantos cofres fueron abiertos
+let puntosTotales=0 //para llevar la cuenta de los puntos que se van ganando
 
 //creo un array con los posibles valores para los cofres
 let valoresCofres=[20, 20, 20, -15, -15, -15, 0, 0, 0]
-
-//creo una función para mostrar el contenido del cofre
-function verPremio(valorCofre){
-
-    let modalPremio=document.getElementById("modalPremio");
-    modalPremio.classList.add("jakarta", "w30", "w80m", "w60t");
-    modalPremio.style.backgroundColor = "white";
-    modalPremio.style.boxShadow="0 0 10px white";
-    modalPremio.style.borderRadius="0.4em";
-
-    let divContenedor=document.createElement("div");
-    divContenedor.classList.add("w100", "centerX", "centerY", "columna", "padding2");
-
-    let mensaje=document.createElement("h3");
-    
-    if(valorCofre==20){
-        mensaje.innerText="¡Felicitaciones!";
-    } else {
-        if(valorCofre==0){
-            mensaje.innerText="¡Seguí intentando!";
-        } else {
-            mensaje.innerText="¡Una lástima!";
-        }       
-    }
-    
-    divContenedor.appendChild(mensaje);
-
-    let texto=document.createElement("p");
-    texto.innerText="Dentro del cofre hay:";
-    texto.classList.add("marginTop1")
-    divContenedor.appendChild(texto)
-
-    let tituloPremio=document.createElement("h2");
-    tituloPremio.innerText=valorCofre;
-    divContenedor.appendChild(tituloPremio);
-
-    let botonAceptar=document.createElement("button");
-    botonAceptar.innerText="ACEPTAR";
-    botonAceptar.style.backgroundColor = "#EBA000";
-    botonAceptar.style.color = "white"
-    botonAceptar.classList.add("w50", "jakarta", "padding1", "marginTop1");
-    botonAceptar.style.borderRadius="0.8em";
-    divContenedor.appendChild(botonAceptar);
-
-    modalPremio.appendChild(divContenedor);
-    modalPremio.showModal();
-
-    function cerrarPremio(){
-        console.log("hideModal llamado");
-        modalPremio.innerHTML="";
-        modalPremio.close();
-
-        //llamo a la funcion definicion partida solo si ya se abrieron los 4 cofres y si ya se cerró el último modal con puntos
-        if(cofresAbiertos==4){
-            setTimeout(definicionPartida, 400);
-        }
-    }
-
-    botonAceptar.addEventListener("click", cerrarPremio)
-    
-}
-
-const nombre = localStorage.getItem("nombre");// Obtengo el nombre almacenado en localStorage
-console.log(nombre); 
-
-//creo una funcion para mostrar los puntos
-function mostrarPuntos(puntosActuales){
-    const contPuntos=document.getElementById("puntos");
-    contPuntos.classList.add("jakarta", "colorBlanco", "puntosClase", "puntosClasem");
-    contPuntos.style.fontSize = "1.4em";
-    contPuntos.innerText=nombre+":  "+puntosActuales
-}
-
-mostrarPuntos(puntosTotales)
-
 
 //Funcion para mostrar el contenedor con los 9 cofres
 function mostrarCofres(){
@@ -89,6 +15,7 @@ function mostrarCofres(){
     let cofres9=document.createElement("div");
     cofres9.classList.add("cofres9", "centerX", "centerY", "spaceb", "w90m", "marginTop5m" , "cofres9m", "w80t", "cofres9t")
 
+    //creo un for para crear los 9 cofres
     for(let i=0; i<9; i++){
 
         let cofreContainer = document.createElement("div");
@@ -109,7 +36,7 @@ function mostrarCofres(){
         imgCofreAbierto.style.visibility = "hidden";
         cofreContainer.appendChild(imgCofreAbierto);
 
-        //selecciono un índice random del array y luego lo elimino para no usar dos vecesel mismo valor
+        //selecciono un índice random del array y luego lo elimino para no usar dos veces el mismo valor
         let indiceRandom=Math.floor(Math.random()*valoresCofres.length);
         imgCofreAbierto.setAttribute("valor", valoresCofres[indiceRandom]);
         valoresCofres.splice(indiceRandom,1)
@@ -127,6 +54,7 @@ function mostrarCofres(){
                 cofresAbiertos++
                 console.log(cofresAbiertos)
 
+                //llamo a la funcion ver premio
                 verPremio(valorAsignado);
 
                 //sumo los puntos
@@ -147,9 +75,92 @@ function mostrarCofres(){
 
 }
 
+//llamo a la funcion cuando carga la página
 document.addEventListener("DOMContentLoaded", mostrarCofres)
 
-const botonReiniciar=document.getElementById("reiniciar");
+
+//creo una función para mostrar el contenido del cofre
+function verPremio(valorCofre){
+
+    let modalPremio=document.getElementById("modalPremio");
+    modalPremio.classList.add("jakarta", "w30", "w80m", "w60t");
+    modalPremio.style.backgroundColor = "white";
+    modalPremio.style.boxShadow="0 0 10px white";
+    modalPremio.style.borderRadius="0.4em";
+
+    let divContenedor=document.createElement("div");
+    divContenedor.classList.add("w100", "centerX", "centerY", "columna", "padding2");
+
+    let mensaje=document.createElement("h3");
+    
+    //genero un mensaje distinto según el valor del contenido del cofre
+    if(valorCofre==20){
+        mensaje.innerText="¡Felicitaciones!";
+    } else {
+        if(valorCofre==0){
+            mensaje.innerText="¡Seguí intentando!";
+        } else {
+            mensaje.innerText="¡Una lástima!";
+        }       
+    }
+    
+    divContenedor.appendChild(mensaje);
+
+    let texto=document.createElement("p");
+    texto.innerText="Dentro del cofre hay:";
+    texto.classList.add("marginTop1")
+    divContenedor.appendChild(texto)
+
+    //agrego el valor del cofre
+    let tituloPremio=document.createElement("h2");
+    tituloPremio.innerText=valorCofre;
+    divContenedor.appendChild(tituloPremio);
+
+    let botonAceptar=document.createElement("button");
+    botonAceptar.innerText="ACEPTAR";
+    botonAceptar.style.backgroundColor = "#EBA000";
+    botonAceptar.style.color = "white"
+    botonAceptar.classList.add("w50", "jakarta", "padding1", "marginTop1");
+    botonAceptar.style.borderRadius="0.8em";
+    divContenedor.appendChild(botonAceptar);
+
+    modalPremio.appendChild(divContenedor);
+    modalPremio.showModal();
+
+    //creo una funcion para cerrar el modal
+    function cerrarPremio(){
+        console.log("hideModal llamado");
+        modalPremio.innerHTML="";
+        modalPremio.close();
+
+        //llamo a la funcion definicion partida solo si ya se abrieron los 4 cofres y si ya se cerró el último modal con puntos
+        if(cofresAbiertos==4){
+            setTimeout(definicionPartida, 400);
+        }
+    }
+
+    botonAceptar.addEventListener("click", cerrarPremio)
+    
+}
+
+
+const nombre = localStorage.getItem("nombre");// Obtengo el nombre almacenado en localStorage
+console.log(nombre); 
+
+//creo una funcion para mostrar los puntos
+function mostrarPuntos(puntosActuales){
+    const contPuntos=document.getElementById("puntos");
+    contPuntos.classList.add("jakarta", "colorBlanco", "puntosClase", "puntosClasem");
+    contPuntos.style.fontSize = "1.4em";
+    contPuntos.innerText=nombre+":  "+puntosActuales
+}
+
+mostrarPuntos(puntosTotales)
+
+
+
+
+const botonReiniciar=document.getElementById("reiniciar"); //me traigo el botón reiniciar
 
 //creo una funcion para reiniciar la partida
 function reiniciarPartida(){
@@ -162,9 +173,10 @@ function reiniciarPartida(){
     mostrarCofres()//llamo a la funcion que cree nuevamente los cofres con su contenido
 }
 
+//le asigno el evento al boton
 botonReiniciar.addEventListener("click", reiniciarPartida)
 
-
+//funcion para definir el resultado de la partida
 function definicionPartida(){
     let modalFinal=document.getElementById("modalFin");
     modalFinal.classList.add("jakarta", "w30","w80m", "w60t", "textCenterm");
